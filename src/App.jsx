a6cfd128df09adbd30a10da8fc9e731206e7bcf2
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-08-04 22:41:53 -0500

    Values.js Library

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import Form from './Form';
 import ColorList from './ColorList';
+import Values from 'values.js';
+
+import { useState } from 'react';
 
 const App = () => {
+  const [colors, setColors] = useState(new Values().all(10));
+
+  console.log(colors);
+
   return (
     <main>
       <Form />
